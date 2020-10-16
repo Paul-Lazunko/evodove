7f1ec38b3b9cf0d968f45d1b0cb16b6b6640e192
pashaman <plavel@gmail.com>
2020-10-16 10:38:20 +0700

    [feature] updated config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import {
   DEFAULT_NO_BRO_SERVER_PORT,
   DEFAULT_NO_BRO_WORKERS_COUNT,
-  DEFAULT_NO_BRO_STORE_RESPONSE_MS
+  DEFAULT_NO_BRO_STORE_RESPONSE_MS,
+  DEFAULT_NO_BRO_DATA_FILENAME,
+  DEFAULT_NO_BRO_DATA_VOLUME
 } from '../constants';
 
 const data: { [key: string]: string | number } = process.env;
@@ -18,5 +20,7 @@ export const config: any = {
   port: process.env.NO_BRO_SERVER_PORT || DEFAULT_NO_BRO_SERVER_PORT,
   workersCount: process.env.NO_BRO_WORKERS_COUNT || DEFAULT_NO_BRO_WORKERS_COUNT,
   storeRequestValueMs: process.env.NO_BRO_STORE_RESPONSE_MS || DEFAULT_NO_BRO_STORE_RESPONSE_MS,
-  secretKey: process.env.NO_BRO_SECRET_KEY
+  dataVolume: process.env.NO_BRO_DATA_VOLUME || DEFAULT_NO_BRO_DATA_VOLUME,
+  dataFileName: DEFAULT_NO_BRO_DATA_FILENAME,
+  secureKey: process.env.NO_BRO_SECRET_KEY
 };
