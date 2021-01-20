a819c07afa577eaf3124169c8a6b41fd3da98948
pashaman <plavel@gmail.com>
2021-01-20 10:02:46 +0200

    chore(test): added jest config

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    globals: {
+        'ts-jest': {
+            tsConfigFile: 'tsconfig.json'
+        }
+    },
+    moduleFileExtensions: [
+        'ts',
+        'js',
+        'json'
+    ],
+    transform: {
+        '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
+    },
+    testMatch: [
+        '**/test/**/*test.(ts|js)'
+    ],
+    testEnvironment: 'node',
+    verbose: true,
+    testURL: "http://localhost:3000/",
+    testTimeout: 30000
+};
